42c752ba985553435e2883591b3882f987d502b2
Aurélien Maury <amaury@xebia.fr>
2012-10-04 18:27:38 +0200

    custom route+static files web server

--- App.groovy
+++ App.groovy
@@ -1,20 +1,57 @@
-def webServerConf = [
-  port: 8081,
-  host: 'localhost',
-  bridge: true,
-  inbound_permitted: [
-    [ address : 'gambit.chat' ]
-  ],
-  outbound_permitted: [
-    [ address : 'gambit.chat' ]
-  ]
-]
+import org.vertx.groovy.core.http.RouteMatcher
 
-container.deployModule('vertx.web-server-v1.0', webServerConf)
+def confOrDefault = { configKey, defaultValue ->
+  container.config[configKey] ?: defaultValue
+}
 
-println "Starting on ${webServerConf.port}..."
+def routeMatcher = new RouteMatcher()
 
-def myHandler = { message -> println "I received a message ${message.body.text}" }
+String webRootPrefix = confOrDefault('web_root', 'web') + File.separator
+String indexPage = webRootPrefix + 'index.html'
+boolean gzipFiles = confOrDefault('gzip_files', false)
 
-def eb = vertx.eventBus
-eb.registerHandler("gambit.chat", myHandler) { println "The handler has been registered across the cluster" }
+/**
+ * Rest routes
+ */
+
+
+
+/**
+ * When no route matches, then serve static files
+ */
+routeMatcher.noMatch { req ->
+  // browser gzip capability check
+  String acceptEncoding = req.headers['accept-encoding']
+  boolean acceptEncodingGzip = acceptEncoding?.contains('gzip')
+
+  String fileName = webRootPrefix + req.path
+
+  if (req.path.contains("..")) {
+    req.response.statusCode = 404
+    req.response.end()
+  } else if (req.path == '/') {
+    req.response.sendFile(indexPage)
+  } else {
+    // try to send *.gz file
+    if (gzipFiles && acceptEncodingGzip) {
+      if (new File(fileName + '.gz').exists()) {
+        // found file with gz extension
+        req.response.headers['content-encoding'] = 'gzip'
+        req.response.sendFile(fileName + '.gz')
+      } else {
+        // not found gz file, try to send uncompressed file
+        req.response.sendFile(fileName)
+      }
+    } else {
+      if (new File(fileName).exists()) {
+        // send not gzip file
+        req.response.sendFile(fileName)
+      } else {
+        req.response.statusCode = 404
+        req.response.end()
+      }
+    }
+  }
+}
+
+vertx.createHttpServer().requestHandler(routeMatcher.asClosure()).listen(confOrDefault('port', 80) as int, confOrDefault("host", "0.0.0.0") as String)
